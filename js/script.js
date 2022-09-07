e47bb0480fc4cfdbc72e41a9aeaef8fe18b83da6
Roy <loganroyjr4@gmail.com>
2022-09-06 20:31:33 -0500

    cleaned up css styling for Nav and Main...created function for questions

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@ var questionBox = document.querySelector("#Question");
 var startQuizButton = document.querySelector(".start-button");
 var timerCountdown = document.querySelector(".timer");
 var remainingTime = 6000;
-var questionIndex = 0; 
-var currentQuestion = document.querySelector('#New-Question') 
-var answerChoices = document.querySelector('#list-of-avaible-answers')
+var questionIndex = 0;
+var currentQuestion = document.querySelector('#New-Question');
+var answerChoices = document.querySelector('#list-of-avaible-answers');
+var initialsBox = document.querySelector('.initialsInput');
+var endOfQuizScreen = document.querySelector('.end-screen');
 
 startQuizButton.addEventListener("click", countDown);
-    function countDown() {
-        startPage.style.display = "none";
-        questionBox.style.display = "block";
-        var countDown = setInterval(function () {
-            if (remainingTime > 1) {
+function countDown() {
+    startPage.style.display = "none";
+    questionBox.style.display = "block";
+    var countDown = setInterval(function () {
+        if (remainingTime > 1) {
             timerCountdown.textContent = remainingTime + ' seconds remaining';
             remainingTime--;
         } else if (remainingTime === 1) {
@@ -22,21 +24,21 @@ startQuizButton.addEventListener("click", countDown);
             timerCountdown.textContent = '';
             clearInterval(countDown);
         }
-        }, 1000);
-        startQuiz();
-    }
+    }, 1000);
+    startQuiz();
+}
 
 
-function startQuiz(){
+function startQuiz() {
     var viewableQuestion = questions[questionIndex];
 
     currentQuestion.textContent = viewableQuestion.question;
 
 
-    for(var i = 0; i < viewableQuestion.choices.length; i++){
+    for (var i = 0; i < viewableQuestion.choices.length; i++) {
         var choice = viewableQuestion.choices[i];
         var choiceBtn = document.createElement('button');
-        choiceBtn.setAttribute('class','choice');
+        choiceBtn.setAttribute('class', 'choice');
         choiceBtn.setAttribute('value', choice);
 
         choiceBtn.textContent = i + 1 + '.' + choice;
@@ -45,4 +47,55 @@ function startQuiz(){
 
     }
 
+}
+
+function nextQuestion(event) {
+    var clicked = event.target;
+
+    if (!clicked.matches('.choice')) {
+        return;
+    }
+
+    if (clicked.value !== questions[questionIndex].answer) {
+        time -= 15;
+    }
+
+    if (remainingTime < 0) {
+        remainingTime = 0;
+    }
+    remainingTime.textContent = remainingTime;
+
+    questionIndex++;
+
+    if (remainingTime <= 0 || questionIndex === questions.length) {
+        endQuiz();
+    } else {
+        // nextQuestion(); am i calling the wrong function?
+    }
+
+}
+
+function saveScore(){
+    var initials = initialsBox.value;
+
+    if (initials !== '') {
+        var score = JSON.parse(window.localStorage.getItem('HIGH SCORES')) || [];
+
+        var newScore = {
+            score: remainingTime,
+            initials: initials,
+        };
+
+        score.push(newScore);
+        window.localStorage.setItem('score', JSON.stringify(score));
+
+        window.localStorage.href = 'highscoresHistory.html';
+
+    }
+}
+
+function checkForSubmission(event) {
+    if (event.key === 'Enter'){
+        saveScore();
+    }
 }
